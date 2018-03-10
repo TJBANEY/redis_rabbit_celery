0ae9dc4f9e8e6112fc71d61e45077c9db4736d82
Timothy Baney <baney-surv@hotmail.com>
2018-03-10 13:17:26 -0600

    move all files into django project

--- celery_redis_rabbit_tut/wikipedia/tasks.py
+++ celery_redis_rabbit_tut/wikipedia/tasks.py
@@ -1,6 +1,7 @@
 # from celery_redis_rabbit_tut.celery import app
 from celery import Celery
 import time
+import bs4
 
 from wikipedia.models import WikipediaPage
 
@@ -14,14 +15,19 @@ app.conf.beat_schedule = {
 }
 
 @app.task()
-def inspect_page():
-    # Grabs all wiki pages that haven't been scanned by Beautiful Soup
-    pages = WikipediaPage.objects.filter(scanned=False)
+def grab_idle_pages():
+    # Grabs Idle Pages, and throw them in Queue to be scanned
+    pages = WikipediaPage.objects.filter(scanned='I')
 
     print('This Works')
 
+def scan_page(idle_pages):
+    for page in idle_pages:
+        pass
+
+
 count = 1
 while count < 10:
-    inspect_page.delay()
+    grab_idle_pages.delay()
     time.sleep(2)
     count += 1
